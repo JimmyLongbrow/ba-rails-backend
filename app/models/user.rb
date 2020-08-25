71c5c5fb8e2cc256f692003d86f510f6590366e6
JimmyLongbrow <organixaustralia@live.com>
2020-08-25 20:21:08 +1000

    ':default => admin'

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   has_many: reservations
   has_many: flights
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_25_094807) do
+ActiveRecord::Schema.define(version: 2020_08_25_101730) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,9 +44,9 @@ ActiveRecord::Schema.define(version: 2020_08_25_094807) do
 
   create_table "users", force: :cascade do |t|
     t.text "name"
-    t.boolean "admin"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
   end
 
 end
