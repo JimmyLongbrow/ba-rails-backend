36f8cc6ce3594b81dd59e61e37dc68b41962fc04
JimmyLongbrow <organixaustralia@live.com>
2020-08-26 10:06:10 +1000

    'changed plane_id to airplane_id from Flights'

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_25_101730) do
+ActiveRecord::Schema.define(version: 2020_08_26_000439) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,12 +25,12 @@ ActiveRecord::Schema.define(version: 2020_08_25_101730) do
 
   create_table "flights", force: :cascade do |t|
     t.text "date"
-    t.integer "plane_id"
     t.integer "flight_number"
     t.text "origin"
     t.text "destination"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "airplane_id"
   end
 
   create_table "reservations", force: :cascade do |t|
