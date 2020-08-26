bf20226f356225e0ee1f7d7d68a526ca2ac2b6b5
Danny Lai <dannyphlai@gmail.com>
2020-08-26 12:46:00 +1000

    new, destroy, flight_params

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,14 +2,25 @@ class FlightsController < ApplicationController
 
   skip_before_action :verify_authenticity_token, raise: false
 
+  def new
+    @airplane = Airplane.new
+  end
+
+  def create
+    @airplane = Airplane.create content: params[:content]
+  end
+
   def index
     render json: Flight.all
   end
 
+  def destroy
+  end
 
-  def create
-    airplane = Airplane.create content: params[:content]
-    render json: airplane
+  private
+
+  def flight_params
+    params.require(:flight).permit(:date, :flight_number, :origin, :destination, :airplane_id)
   end
 
 end
